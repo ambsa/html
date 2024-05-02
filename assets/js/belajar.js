cdfc87d7a3e3c869086b6d83701e33d3036e6acc
ambsa <fatahahmad807@gmail.com>
2024-05-02 08:09:06 +0700

    add halaman tentang with css and js, and repair all page

--- assets/js/belajar.js
+++ assets/js/belajar.js
@@ -1,9 +1,74 @@
-$(document).ready(function() {
-    $(".slide-container").on("mousewheel", function(event) {
-        event.preventDefault();
-        const scrollAmount = 300; // Atur jumlah scroll di sini
-        const container = $(this);
-        const currentScrollLeft = container.scrollLeft();
-        container.scrollLeft(currentScrollLeft + (event.originalEvent.deltaY * scrollAmount));
+
+// card swiper
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    slidesPerGroup: 3,
+    loop:true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor:'true',
+    loopFillGroupWithBlank:true,
+    pagination:{
+    el:".swiper-pagination",
+    clickable: true,
+    dynamicBullets:true,
+    },
+    navigation:{
+    nextEl:".swiper-button-next",
+    prevEl:".swiper-button-prev"
+    },
+    breakpoints:{
+    0:{
+        slidesPerView:1,
+    },
+    520:{
+        slidesPerView:2,
+    },
+    950:{
+        slidesPerView:3,
+    },
+    },
     });
-});
+
+     // Fungsi untuk mengarahkan halaman ke atas
+     function scrollToTop() {
+        // Mendapatkan elemen <body>
+        const body = document.body;
+        // Mendapatkan elemen <html>
+        const html = document.documentElement;
+    
+        // Menggunakan animasi scroll smooth (opsional)
+        body.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+        html.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+    
+    // Memeriksa posisi scroll untuk menampilkan tombol
+    window.onscroll = function () {
+        scrollFunction();
+    };
+    
+    function scrollFunction() {
+        const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    
+        // Jika pengguna telah melakukan scroll lebih dari 20 piksel dari bagian atas halaman, tombol akan ditampilkan
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            // Jika belum, tombol akan disembunyikan
+            scrollToTopBtn.style.display = "none";
+        }
+    }
+
+    
+
+    
+
+
+     
